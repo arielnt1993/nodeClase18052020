47a5fd7ff953ca7ed99a687a0e49fb248a8779c2
arielnt1993 <61444560+arielnt1993@users.noreply.github.com>
2020-05-18 08:56:41 -0300

    clase de node 18052020 viendo modulos 3

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const logger = require('./logger');
 const path = require('path');
-
+const os = require('os');
 let pathObject = path.parse(__filename);
 
-//console.log(pathObject);
-
 logger.log(pathObject);
+
+let freeMem = os.freemem();
+let totalMem = os.totalmem();
+
+console.log('memoria libre',freeMem);
+console.log('memoria total',totalMem);
+
+logger.log(freeMem);
+logger.log(totalMem);
