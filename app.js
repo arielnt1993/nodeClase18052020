75626cfe2d8b78447037fdffe408b1f401078058
arielnt1993 <61444560+arielnt1993@users.noreply.github.com>
2020-05-18 09:05:16 -0300

    clase de node 18052020 viendo modulos 4

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const logger = require('./logger');
 const path = require('path');
 const os = require('os');
+const fs = require('fs');
+
 let pathObject = path.parse(__filename);
 
 logger.log(pathObject);
@@ -13,3 +15,11 @@ console.log('memoria total',totalMem);
 
 logger.log(freeMem);
 logger.log(totalMem);
+
+//forma sincrona esta "mal"
+let rootDir = fs.readdirSync('./');
+console.log(rootDir);
+
+fs.readdir('./',(e,files) => {
+    (e) ? console.log(e) : console.log(files);
+});
